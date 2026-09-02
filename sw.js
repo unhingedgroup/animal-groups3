83ed8deaac103ae60ae52ca161396750f74e29de
unhingedgroup <crazyarchitect.13@gmail.com>
2026-09-02 02:22:13 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='animal-groups-fun-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
